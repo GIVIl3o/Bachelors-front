3e42f199ad8282a5e16bc143f67d1a53b91416bb
Leo Sarchimelia <GIVIl3o5@gmail.com>
2020-03-28 23:38:27 +0400

    added backend url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { StylesProvider } from "@material-ui/core/styles";
+import axios from "axios";
 
 function App() {
+  const [test, setTest] = useState("");
+
+  const callBackend = async () => {
+    const response = await axios.get("/retrieve?id=2");
+    console.log(response);
+    setTest(response.data.testField);
+  };
+
   return (
     <StylesProvider injectFirst>
       <h1 style={{ color: "blue" }}>This almost looks fun</h1>
+      <input type="button" onClick={callBackend} value="Ask backend" />
+      {test === "" ? (
+        ""
+      ) : (
+        <h2 style={{ color: "red" }}>Backend replied with:{test}</h2>
+      )}
     </StylesProvider>
   );
 }
