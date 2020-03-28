8bd6fa3072569b7487adb32643187ce306d21a1f
Leo Sarchimelia <GIVIl3o5@gmail.com>
2020-03-29 00:33:14 +0400

    backend url bugfix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+import { proxy } from "../package.json";
+
+if (process.env.NODE_ENV === "production") axios.defaults.baseURL = proxy;
 
 ReactDOM.render(<App />, document.getElementById("root"));
