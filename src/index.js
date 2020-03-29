3115f783902a569a5b6afcd3a7526f0587a76aac
Leo Sarchimelia <GIVIl3o5@gmail.com>
2020-03-29 20:22:39 +0400

    small update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import axios from "axios";
-import { proxy } from "../package.json";
+import { baseUrl } from "../package.json";
 
-if (process.env.NODE_ENV === "production") axios.defaults.baseURL = proxy;
+axios.defaults.baseURL = baseUrl;
 
 ReactDOM.render(<App />, document.getElementById("root"));
