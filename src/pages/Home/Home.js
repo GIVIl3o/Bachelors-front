b711cdf834456f938b04a39d85c6f7fd76aa554a
Maria Tarielashvili <mtari11@freeuni.edu.ge>
2020-05-10 02:30:03 +0400

    cleanup + authenticated home started

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,29 @@
 import React, { useContext } from "react";
 import { UserContext } from "App";
+import { currentlyHave } from "./Authenticated.module.css";
+import Projects from "./Projects";
 
 const Home = () => {
-  const { setUsername } = useContext(UserContext);
+  const { setUsername, text } = useContext(UserContext);
 
   const logout = () => {
     localStorage.removeItem("jwt");
     setUsername(undefined);
   };
-
+  document.title = text.home_title;
   return (
     <div style={{ alignSelf: "baseline" }}>
-      <input
-        type="submit"
-        onClick={logout}
-        value="Logout"
-        style={{ margin: "20px" }}
-      />
+      <div>
+        temporary:
+        <input
+          type="submit"
+          onClick={logout}
+          value="Logout"
+          style={{ margin: "20px" }}
+        />
+      </div>
+      <span className={currentlyHave}>{text.currently_have}</span>
+      <Projects />
     </div>
   );
 };
